f0a3abc3316b695828b1c071c8c7ef7c8fe1911f
Anders Malling privat <Andersmalling@gmail.com>
2023-10-02 21:57:48 +0200

    working on loading page rescaling imgs

--- src/App.jsx
+++ src/App.jsx
@@ -3,65 +3,112 @@ import MyFooter from "./components/footer.jsx";
 /// skift alle links til assests når der skal buildes
 
 export default function App() {
-return (
-<div className=" w-screen h-screen">
-{/*     <h1 className="py-4 mt-0 shadow-sm text-3xl font-bold underline justify-center text-center items-start">
+  return (
+    <div className=" w-screen h-screen">
+      {/*     <h1 className="py-4 mt-0 shadow-sm text-3xl font-bold underline justify-center text-center items-start">
                   Anders Helle Malling alling dev yooohuu - tag lidt fra den gamle repo
                 </h1> */}
-<div className="flex min-h-full items-center justify-center">
-<div className="shadow drop-shadow-2xl p-2 ">
-<img className="h-96 border-2 border-gray-800 rounded-full rounded-tr-3xl" src="src\assets\ANDMAL.jpg" alt="billede af Anders"></img>
-<p className="text-lg font-semibold ">Anders Helle Malling</p>
-<p className="text-md italic">Fullstack Webdeveloper</p>
-<div className="text-xs">React, Vuejs, Typescript, Tailwind, Flask, SQLserver</div>
+      <div className="flex min-h-full items-center justify-center">
+        <div className="shadow drop-shadow-2xl p-2 ">
+          <img
+            className="h-96 border-2 border-gray-800 rounded-full rounded-tr-3xl"
+            src="src\assets\ANDMAL.jpg"
+            alt="billede af Anders"
+          ></img>
+          <p className="text-lg font-semibold ">Anders Helle Malling</p>
+          <p className="text-md italic">Fullstack Webdeveloper</p>
+          <div className="text-xs">
+            React, Vuejs, Typescript, Tailwind, Flask, SQLserver
+          </div>
+        </div>
+      </div>
+      <h2 className="text-center text-2xl font-bold ">Portfolio:</h2>
+      <div className="flex min-h-full items-start mt-12">
+        <div className="grid grid-cols-2 items-center drop-shadow-2xl p-2">
+          <div className="flex flex-col px-3 ml-52 justify-center h-96">
+            <h3 className="text-xl left font-bold ">AuhKraeft.rm.dk </h3>
+            <div className=" italic ">
+              auctor augue mauris augue neque gravida in fermentum et
+              sollicitudin ac orci phasellus egestas tellus rutrum tellus
+              pellentesque eu tincidunt tortor aliquam nulla facilisi cras
+              fermentum odio eu feugiat pretium nibh ipsum consequat nisl
+            </div>
+          </div>
 
-</div>
-</div>
-<h2 className="text-center text-2xl font-bold ">Portfolio:</h2>
-<div className="flex min-h-full items-center justify-center">
-
-<div className="grid grid-cols-2 items-center drop-shadow-2xl p-2">
-<span className="col-span-1 ml-52 px-3 italic ">auctor augue mauris augue neque gravida
- in fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque 
- eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl</span>
-
-<img className="col-span-1 h-96 mr-52 px-3 border-2 border-gray-800" src="src\assets\ss-webapp1.png" alt="Screenshot of med app" />
-<div className="col-span-1"></div>
-<div className="flex py-2">
-<p className="mr-2 ">Stack: </p>
-<img src="src\assets\vuejs icon.jpg" className="h-16 mr-1 rounded-full"  alt="vueicon" />
-<img src="src\assets\typescript icon.png" className="h-16 mr-1 rounded-full" alt="tsicon" />
-<img src="src\assets\tailwind icon.png" className="bg-white h-16 rounded-full" alt="twicon" />
-
-</div>
-
-</div>
-
-</div>
-<div className="grid grid-cols-2 drop-shadow-2xl p-2">
-<div className="ml-52">
-<img className="col-span-1 h-96  px-3 border-2 border-gray-800" src="src\assets\ss-webapp2.png" alt="Screenshot of med app" />
-<div className="col-span-1"></div>
-<div className="flex py-2">
-<p className="mr-2 ">Stack: </p>
-<img src="src\assets\react-icon.png"  className="bg-white h-16 mr-1 rounded-full" alt="vueicon" />
-<img src="src\assets\typescript icon.png" className="h-16 mr-1 rounded-full" alt="tsicon" />
-<img src="src\assets\tailwind icon.png" className="bg-white h-16 rounded-full" alt="twicon" />
-
-</div>
-</div>
-<div className="flex flex-col px-3 mr-52 justify-center h-96">
-<h3 className="text-xl left font-bold ">BajerBixen.dk </h3>
-<div className=" italic ">auctor augue mauris augue neque gravida in fermentum et
-sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam
-nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl
-</div>
-</div>
-
-
-
-</div>
-<MyFooter />
-</div>
-);
+          <img
+            className="col-span-1 h-96 mr-52 px-3 border-2 border-gray-800"
+            src="src\assets\ss-webapp1.png"
+            alt="Screenshot of med app"
+          />
+          <div className="col-span-1"></div>
+          <div className="flex py-2">
+            <p className="mr-2 ">Stack: </p>
+            <img
+              src="src\assets\vuejs icon.jpg"
+              className="h-16 mr-1 rounded-full"
+              alt="vueicon"
+            />
+            <img
+              src="src\assets\typescript icon.png"
+              className="h-16 mr-1 rounded-full"
+              alt="tsicon"
+            />
+            <img
+              src="src\assets\tailwind icon.png"
+              className="bg-white h-16 rounded-full"
+              alt="twicon"
+            />
+            <img
+              src="src\assets\github_logo.png"
+              className="bg-white h-16 rounded-full ml-96"
+              alt="github logo and link"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 drop-shadow-2xl p-2">
+        <div className="ml-52">
+          <img
+            className="col-span-1 h-96 px-3 border-2 border-gray-800"
+            src="src\assets\ss-webapp2.png"
+            alt="Screenshot of med app"
+          />
+          <div className="col-span-1"></div>
+          <div className="flex py-2">
+            <p className="mr-2 ">Stack: </p>
+            <img
+              src="src\assets\react-icon.png"
+              className="bg-white h-16 mr-1 rounded-full"
+              alt="vueicon"
+            />
+            <img
+              src="src\assets\typescript icon.png"
+              className="h-16 mr-1 rounded-full"
+              alt="tsicon"
+            />
+            <img
+              src="src\assets\tailwind icon.png"
+              className="bg-white h-16 rounded-full"
+              alt="twicon"
+            />
+            <img
+              src="src\assets\github_logo.png"
+              className="bg-white h-16 rounded-full ml-96"
+              alt="github logo and link"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col px-3 mr-52 justify-center h-96">
+          <h3 className="text-xl left font-bold ">BajerBixen.dk </h3>
+          <div className=" italic ">
+            auctor augue mauris augue neque gravida in fermentum et sollicitudin
+            ac orci phasellus egestas tellus rutrum tellus pellentesque eu
+            tincidunt tortor aliquam nulla facilisi cras fermentum odio eu
+            feugiat pretium nibh ipsum consequat nisl
+          </div>
+        </div>
+      </div>
+      <MyFooter />
+    </div>
+  );
 }
