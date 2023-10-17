e9bb7b7da2c9848cf210084aaa09950cbe9824a0
Anders Malling privat <Andersmalling@gmail.com>
2023-10-17 21:42:20 +0200

    added content to landingpage and change stack icon

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,27 @@ export default function App() {
     <div className=" w-screen h-screen">
       <MyNavBar />
 
-      <div className="flex h-5/6 mb-24 items-center justify-center">
-        <div className="w-3/5 h-auto grid grid-cols-2   p-2 ">
+      <div className="flex h-full mb-24 items-center justify-center">
+        <div className="w-4/5 h-auto grid grid-cols-2   p-2 ">
           <div className="flex-row items-center justify-center  p-5">
             <p className="text-4xl font-bold mt-5">Fullstack Webdeveloper</p>
-            <p className="text-lg font-semibold text text-gray-500 mt-4">
-              Hello! im Anders Helle Malling a passioned webDeveloper based in
-              Aarhus, Denmark
+            <p className="text-lg font-semibold text-gray-500 mt-4">
+              Welcome to Vanaheim, your go-to destination for all your web
+              development needs! I'm Anders Helle Malling, a passionate and
+              dependable web developer based in Aarhus, Denmark, and I run this
+              solo web agency.
+              <p className="mt-3">
+                At Vanaheim, I take on projects of all sizes. Whether you
+                require minor bug fixes, additional functionalities for your
+                existing codebase, or a brand-new web application or webshop
+                built from scratch, I've got you covered.
+              </p>
+              <p className="mt-3">
+                But don't just take my word for it. Explore my portfolio to see
+                for your self. With Vanaheim, you can trust that your web
+                projects are in save hands, and I'm here to turn your digital
+                aspirations into reality.
+              </p>
             </p>
           </div>
           <img
@@ -23,7 +37,8 @@ export default function App() {
             alt="billede af Anders"
           ></img>
           <div className="text ml-5 col-span-2">
-            Tech Stack : React, Vuejs, Typescript, Tailwind, Flask, SQLserver
+            Tech Stack : React, Vuejs, Typescript, Nextjs, Tailwind, Flask,
+            MongoDB
           </div>
         </div>
       </div>
@@ -98,9 +113,9 @@ export default function App() {
               alt="mongodb icon"
             />
             <img
-              src="assets\express-logo.png"
+              src="assets\next-js_icon.png"
               className="h-16 mr-1 rounded-full bg-white"
-              alt="express icon"
+              alt="nextjs icon"
             />
             <img
               src="assets\tailwind icon.png"
@@ -127,7 +142,7 @@ export default function App() {
       <div className="flex items-start mt-12">
         <div className="grid grid-cols-2 items-center drop-shadow-2xl p-2">
           <div className="flex flex-col px-3 ml-52 justify-center h-96">
-            <h3 className="text-xl left font-bold ">PsykologNanna.dk </h3>
+            <h3 className="text-xl left font-bold ">PsykologMalling.dk </h3>
             <div className=" italic ">
               auctor augue mauris augue neque gravida in fermentum et
               sollicitudin ac orci phasellus egestas tellus rutrum tellus
@@ -155,7 +170,7 @@ export default function App() {
                 alt="tsicon"
               />
               <img
-                src="assets\express-logo.png"
+                src="assets\next-js_icon.png"
                 className="h-16 mr-1 rounded-full bg-white"
                 alt="express icon"
               />
