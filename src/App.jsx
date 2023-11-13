5c201e17aa20182bf76ee601cac793fcb1a89318
Anders Malling privat <Andersmalling@gmail.com>
2023-11-13 20:45:18 +0100

    changed link

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ export default function App() {
           <div className="md:ml-52">
             <a
               className="cursor-pointer"
-              href="https://bajer-bixen.vercel.app/"
+              href="https://bajer-bixen.vercel.app/julekalenderen"
             >
               <img
                 className="col-span-1 w-11/12 border-2 border-gray-800"
